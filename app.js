a731148ed77a5e8daeab641d897898520c88281d
George Rowlands <rowla101@gmail.com>
2019-12-31 22:39:21 +0100

    Finished swagger docs

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var logger = require('morgan');
-var fs = require('fs')
-var app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const fs = require("fs");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const app = express();
 
-const port = 3003
+const port = 3003;
 
 // configure body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // configure logger
-app.use(logger('common', {
-    stream: fs.createWriteStream('./access.log', { flags: 'a' })
-}));
-app.use(logger('dev'));
+app.use(
+  logger("common", {
+    stream: fs.createWriteStream("./access.log", { flags: "a" })
+  })
+);
+app.use(logger("dev"));
 
 app.get("/", (req, res) => {
-    res.send("Welcome to root!")
-})
+  res.send("Welcome to root!");
+});
 
-const UserRouter = require('./routes/user.js')
+fs.readdir("./routes", (err, files) => {
+  files.forEach(file => {
+    app.use("/", require("./routes/" + file));
+  });
+});
 
-app.use(UserRouter)
+// configure and setup swagger
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Rest API",
+      description: "A simple rest API",
+      servers: ["http://localhost:3003"]
+    }
+  },
+  apis: ["./routes/*.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(port, () => {
-    console.log('Server running on port ' + port);
-})
+  console.log("Server running on port " + port);
+});
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,119 @@ const express = require("express");
 const router = express.Router();
 const getConnection = require("../db.js");
 
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: User management
+ */
+
+/**
+ * @swagger
+ * /user:
+ *    get:
+ *      description: Use to return all users
+ *      tags: [User]
+ *      responses:
+ *        '200':
+ *          description: Successfully returned all users
+ *        '500':
+ *          description: Failed to query for users
+ *    post:
+ *      description: Use to insert a new user
+ *      tags: [User]
+ *      parameters:
+ *        - name: firstName
+ *          in: body
+ *          description: Firstname of the user
+ *          required: true
+ *          schema:
+ *            type: string
+ *        - name: lastName
+ *          in: body
+ *          description: Lastname of the user
+ *          required: true
+ *          schema:
+ *            type: string
+ *      responses:
+ *        '200':
+ *          description: Successfully inserted a user
+ *        '500':
+ *          description: Failed to insert a user
+ */
 router
   .route("/user")
   .get((req, res) => getAllUsers(req, res))
   .post((req, res) => insertUser(req, res));
+
+/**
+ * @swagger
+ * /user/{id}:
+ *    get:
+ *      description: Use to return a user
+ *      tags: [User]
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          description: Id of the user
+ *          required: true
+ *          schema:
+ *            type: integer
+ *      responses:
+ *        '200':
+ *          description: Successfully returned the user
+ *        '404':
+ *          description: User with the given Id couldn't be found
+ *        '500':
+ *          description: Failed to query for user
+ *    put:
+ *      description: Use to update a user
+ *      tags: [User]
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          description: Id of the user
+ *          required: true
+ *          schema:
+ *            type: integer
+ *        - name: firstName
+ *          in: body
+ *          description: Firstname of the user
+ *          required: true
+ *          schema:
+ *            type: string
+ *        - name: lastName
+ *          in: body
+ *          description: Lastname of the user
+ *          required: true
+ *          schema:
+ *            type: string
+ *      responses:
+ *        '200':
+ *          description: Successfully returned the user
+ *        '404':
+ *          description: User with the given Id couldn't be found
+ *        '500':
+ *          description: Failed to update for user
+ *    delete:
+ *      description: Use to delete a user
+ *      tags: [User]
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          description: Id of the user
+ *          required: true
+ *          schema:
+ *            type: integer
+ *      responses:
+ *        '200':
+ *          description: Successfully deleted the user
+ *        '404':
+ *          description: User with the given Id couldn't be found
+ *        '500':
+ *          description: Failed to delete the user
+ */
+
 router
   .route("/user/:id")
   .get((req, res) => getUserById(req, res))
@@ -58,9 +167,14 @@ const getUserById = (req, res) => {
   const idUser = req.params.id;
   const queryString = "SELECT * FROM user WHERE idUser = ?";
   connection.query(queryString, [idUser], (err, results, fields) => {
+    console.log(results);
     if (err) {
       console.log("Failed to query for users: " + err);
       res.sendStatus(500);
+      return;
+    } else if (results.length < 1) {
+      console.log("User with id: " + idUser + "couldn't be found");
+      res.status(404).send("No user found");
       return;
     }
     const user = results.map(row => {
@@ -87,6 +201,10 @@ const updateUser = (req, res) => {
         console.log("Failed to update user: " + err);
         res.sendStatus(500);
         return;
+      } else if (results.length < 1) {
+        console.log("User with id: " + idUser + "couldn't be found");
+        res.status(404).send("No user found");
+        return;
       }
       console.log("Updated user with id: ", idUser);
       const user = { userid: idUser, firstName: firstName, lastName: lastName };
@@ -104,6 +222,10 @@ const deleteUserById = (req, res) => {
     if (err) {
       console.log("Failed to delete user: " + err);
       res.sendStatus(500);
+      return;
+    } else if (results.length < 1) {
+      console.log("User with id: " + idUser + "couldn't be found");
+      res.status(404).send("No user found");
       return;
     }
     console.log("Deleted user with id: ", idUser);
